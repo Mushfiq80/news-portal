06bdca2057d15cd180a03dfbec5b2f93d4af1781
Mushfiq80 <aurpon80@gmail.com>
2024-02-01 20:38:36 +0600

    complete left-right nav and headaer from backend server

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -7,11 +7,11 @@ import bgNav from '../../../assets/bg.png'
 const RightNav = () => {
     return (
         <div>
-            <h4>Login With</h4>
+            <h4 className='mt-4'>Login With</h4>
             <Button className='mb-2' variant="outline-primary"><FaGoogle /> Login With Google</Button>
             <Button variant="outline-secondary"><FaGithub /> Login with GitHub</Button>
             <div>
-                <h4>Find Us On</h4>
+                <h4 className='mt-4'>Find Us On</h4>
                 <ListGroup>
                     <ListGroup.Item><FaFacebook></FaFacebook> FaceBook</ListGroup.Item>
                     <ListGroup.Item><FaSquareXTwitter></FaSquareXTwitter> Twitter</ListGroup.Item>
