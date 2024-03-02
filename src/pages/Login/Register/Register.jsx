6c1d32a2f2a383f3975f6e38c0eacb977413c951
Mushfiq80 <aurpon80@gmail.com>
2024-03-02 07:15:09 +0600

    short go. Night rush

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,50 +1,75 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
 
 const Register = () => {
-    return (
+    const { createUser } = useContext(AuthContext);
+
     
-            <Container className='w-50 mx-auto'>
-                <h2>Register Here!</h2>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Your Name</Form.Label>
-                        <Form.Control type="text" name='name' placeholder="Enter Name" required />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Photo URL</Form.Label>
-                        <Form.Control type="text" name='photo' placeholder="Paste Photo URL" required />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" name='email' placeholder="Enter email" required />
-                    </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" name='password' placeholder="Password" required />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Accept Terms and Conditions" />
-                    </Form.Group>
-                    <Link></Link>
-                    <Button variant="primary" type="submit">
-                        Register
-                    </Button>
-                    <br />
-                    <Form.Text className="text-secondary">
-                        Already Have an Account?<Link to='/login'> Login</Link>
-                    </Form.Text>
-                    <Form.Text className="text-success">
+    const handleRegister = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const photo = form.photo.value;
+        
+        // console.log(name, email, password, photo)
 
-                    </Form.Text>
-                    <Form.Text className="text-danger">
+        createUser(email, password)
+        .then(result => {
+            const createdUser = result.user;
+            console.log(createdUser);
+        })
+        .catch(error => {
+            console.log(error);
+            
+        });
+    }
+    return (
 
-                    </Form.Text>
-                </Form>
-            </Container>
-     
+        <Container className='w-50 mx-auto'>
+            <h2>Register Here!</h2>
+            <Form onSubmit={handleRegister}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter Name" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Photo URL</Form.Label>
+                    <Form.Control type="text" name='photo' placeholder="Paste Photo URL" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Accept Terms and Conditions" />
+                </Form.Group>
+                <Link></Link>
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+                <br />
+                <Form.Text className="text-secondary">
+                    Already Have an Account?<Link to='/login'> Login</Link>
+                </Form.Text>
+                <Form.Text className="text-success">
+
+                </Form.Text>
+                <Form.Text className="text-danger">
+
+                </Form.Text>
+            </Form>
+        </Container>
+
     );
 };
 
